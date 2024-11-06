ec4f4c18dcc9a6d411688af04a81718d8e70b5dd
Júlia Moreira <160127568+a-juliamoreira@users.noreply.github.com>
2024-11-06 13:04:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ export default function App(){
     <div>
       <Profile 
         avatar={'../../public/avatar.png'}
-        name={'Carlos Daniel'}
-        bio={'Jogador de futebol'}
-        email={'carlos.uchoa2@aluno.ce.gov.br'}
+        name={'júlia Moreira'}
+        bio={'Maquiadora'}
+        email={'juli.costa@aluno.ce.gov.br'}
         phone={'+55 85 99256-3565'}
-        githubUrl={'https://github.com/CarloosDaniel'}
-        instagramUrl={'https://instagram.com/carlosdaniel.sx'}
-        twitterUrl={'https://x.com.br/CarloosDaniel'}
+        githubUrl={'https://github.com/a-juliamoreira'}
+        instagramUrl={'https://instagram.com/_juliacoss'}
+        twitterUrl={'https://x.com.br/juliacoss'}
       />
     </div>
   )
